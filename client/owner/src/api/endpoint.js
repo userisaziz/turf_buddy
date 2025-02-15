17a67a7d045bf57896aba0350ee4f0862a45909a
Abdul Aziz Ahmed <azizadnan370@gmail.com>
2025-02-15 15:19:58 +0530

    owner timeslots verification

--- client/owner/src/api/endpoint.js
+++ client/owner/src/api/endpoint.js
@@ -1,5 +1,6 @@
 export const CREATE_ORDER = "/api/owner/bookings/create-order"
 export const VERIFY_PAYMENT = "/api/owner/bookings/verify-payment"
+export const VERIFY_TIMESLOT = "/api/owner/auth/bookings/verify-timeSlots"
 
 
 export const FORGOT_PASSWORD = "/api/owner/auth/forgot-password"
--- server/routes/owner/bookings.routes.js
+++ server/routes/owner/bookings.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getOwnerBookings, bookTurfForUser, getBookingsByTurfAndDate, createOrder, verifyPayment, deleteBooking } from "../../controllers/owner/booking.controller.js";
+import { getOwnerBookings, bookTurfForUser,verifyTimeSlotAvailability, getBookingsByTurfAndDate, createOrder, verifyPayment, deleteBooking } from "../../controllers/owner/booking.controller.js";
 import verifyOwnerToken from "../../middleware/jwt/owner.middleware.js";
 
 const bookingsRouter = express.Router();
 bookingsRouter.get("/", verifyOwnerToken, getOwnerBookings);
+bookingsRouter.get("/verify-timeSlots", verifyOwnerToken, verifyTimeSlotAvailability);
 bookingsRouter.post("/create-order", verifyOwnerToken, createOrder);
 bookingsRouter.post("/verify-payment", verifyOwnerToken, verifyPayment);
 
